5e2fb68b29b44a22b1b9dc9649cf18785ddb1920
Tiaan <tiaan2000k@gmail.com>
2024-06-23 21:12:26 +0200

    Added: UserLink Model

---
+++ database/migrations/2024_06_23_185552_create_user_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_links', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id','id')->references('id')->on('user');
+            $table->string('code')->unique(); // unique identifier
+            $table->string('url'); // original url
+            $table->string('qrcode_location')->nullable();
+            $table->string('password')->nullable();
+            $table->integer('clicks')->default(1);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_links');
+    }
+};
